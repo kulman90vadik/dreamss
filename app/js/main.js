b1080e5c42081daa168cb2923395e299ba18fa27
Vadim Kulman <vadim.kulman90@mail.ru>
2021-06-24 02:57:57 +0300

    added articles, discount

--- app/js/main.js
+++ app/js/main.js
@@ -34,6 +34,68 @@ $(function(){
         nextArrow: '<button type="button" class="slick-next"><svg class="packages-destinations__arrow"><use xlink:href="images/sprite.svg#chevron-right"></use></svg></button>'
     });
 
+    $('.honeymoon-slider__star').rateYo({
+        starWidth: "13px",
+        normalFill: "#ccc",
+        ratedFill: "#ffc001",
+        readOnly: true,
+        spacing: "7px"
+    });
+
+    $('.honeymoon-slider').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+    });
+
+
+
+    function getTimeRemaining(endtime) {
+        const total = Date.parse(endtime) - Date.parse(new Date());
+        const seconds = Math.floor((total / 1000) % 60);
+        const minutes = Math.floor((total / 1000 / 60) % 60);
+        const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+        const days = Math.floor(total / (1000 * 60 * 60 * 24));
+        
+        return {
+            total,
+            days,
+            hours,
+            minutes,
+            seconds
+        };
+    }
+
+    function initializeClock(id, endtime) {
+        const clock = document.getElementById(id);
+        const daysSpan = clock.querySelector('.days');
+        const hoursSpan = clock.querySelector('.hours');
+        const minutesSpan = clock.querySelector('.minutes');
+        const secondsSpan = clock.querySelector('.seconds');
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            daysSpan.innerHTML = t.days;
+            hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+            minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+            secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+            if (t.total <= 0) {
+                clearInterval(timeinterval);
+            }
+        }
+
+        updateClock();
+        const timeinterval = setInterval(updateClock, 1000);
+    }
+
+    const deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+    initializeClock('discount-clock', deadline);
+
+
+
 
     var mixer = mixitup('.reservation__inner');
     mixer.filter('.hotel');
